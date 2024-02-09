748c3408b35530a2240e34a160be06d79589acc0
Samuel Thomas <samueldthomas91@gmail.com>
2024-02-09 13:11:00 +0000

    added all routing for main navigation. It may change but confirmed to be working successfully with flask and jinja templating

--- app.py
+++ app.py
@@ -23,6 +23,30 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/topics")
+def topics():
+    """
+    Navigate to the topics page
+    """
+    return render_template("topics.html")
+
+
+@app.route("/signup")
+def signup():
+    """
+    Navigate to the signup page
+    """
+    return render_template("signup.html")
+
+
+@app.route("/login")
+def login():
+    """
+    Navigate to the login page
+    """
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
